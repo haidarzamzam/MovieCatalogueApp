ba6d962c21cfaca5064e4e0f9567ec12e1636289
Haidar Zamzam <haidar.zamzam99@gmail.com>
2021-06-06 20:25:52 +0700

    :sparkles: Add model response

--- app/src/main/java/com/haidev/moviecatalogueapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/haidev/moviecatalogueapp/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.haidev.moviecatalogueapp
+package com.haidev.moviecatalogueapp.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.haidev.moviecatalogueapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
